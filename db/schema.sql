b9e5f2166487732651c639092208a7d74d07d9ab
Juan Padilla <jxpadilla@yahoo.com>
2016-11-14 11:33:08 -0600

    db Schema update

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,19 @@
 #MYSQL SCHEMA GOES HERE
+CREATE DATABASE anagram_db;
+
+USE anagram_db;
+
+CREATE TABLE anagram_db.player (
+	playerId INTEGER ( 10 ) AUTO_INCREMENT NOT NULL,
+	playerNumbr INTEGER ( 10 ),
+	playerName VARCHAR ( 255 ),
+	playerScore INTEGER ( 10 ),
+	PRIMARY KEY (playerId));
+
+CREATE TABLE anagram_db.game (
+	gameId INTEGER ( 11 ) AUTO_INCREMENT NOT NULL,
+	gameDesc VARCHAR ( 120 ),
+	gameStartTime TIMESTAMP,
+	gameEndtTime TIMESTAMP,
+	gameTimeLimit INTEGER ( 10),
+	PRIMARY KEY (gameId));
