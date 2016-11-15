e5048deb5d6f4da3a2e987aa2b7f6352e8762ed0
Corey Hickman <CoreyDH@users.noreply.github.com>
2016-11-14 19:58:06 -0600

    Update word.js

--- models/word.js
+++ models/word.js
@@ -22,6 +22,23 @@ var word = {
 		orm.delete('words', id, function(err, data) {
 			callback(data);
 		});
+	},
+	submitScore: function(playerName, score, callback) {
+		orm.all('top_score', function(data) {
+			for(var i=0; i < data.length; i++) {
+				if(score > data[i].score) {
+					// enter into database
+					
+					// if database.length is greater than 50, drop last row from table
+				}
+			}
+		}
+	},
+	retrieveHighScores: function(callback) {
+		// retrieve high score and return into callback function ordered by descending order.
+	},
+	storePlayer: function(playerName, score, callback) {
+		// add player to database
 	}
 };
 
