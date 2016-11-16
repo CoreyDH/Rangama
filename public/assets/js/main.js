539094066130b9a52ba433d45d4de4f387807c46
Corey H <rewer6@gmail.com>
2016-11-15 21:36:11 -0600

    adding more keyup functionality

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -4,6 +4,8 @@ $(function () {
         var Rangama = function (playerName) {
             this.anagram = {};
             this.chosen = [];
+            this.score = 0;
+            this.bestWord = '';
 
             this.newAnagram();
         };
@@ -26,6 +28,7 @@ $(function () {
                 console.log(data);
                 self.anagram = data;
                 self.chosen = data.word.split('');
+                self.word = data.word;
 
                 return data;
             })
@@ -61,18 +64,32 @@ $(function () {
                 var key = String.fromCharCode(event.which).toLowerCase();
                 var keyCode = event.which;
                 var keyIndex = self.chosen.indexOf(key);
-                var allowedSpecialKeys = [8, 46, 45, 13, 127, 37, 38, 39, 40, 16]; // Backspace, Delete, Arrow Keys, Insert
+                var deleteKeys = [8, 46];
+                var otherKeys = [45, 13, 127, 37, 38, 39, 40, 16];
+                var isDeleteKey = deleteKeys.indexOf(keyCode) !== -1; // Backspace, Delete
+                var isOtherKey = otherKeys.indexOf(keyCode) !== -1; //  Arrow Keys, Insert
+                var isSpecialKey = otherKeys.concat(deleteKeys).indexOf(keyCode) !== -1; // Any special key
                 var $textInput = $(this).val();
 
                 if (
                     keyIndex !== -1 // Check if key exists in word.
                     && $textInput.length <= self.anagram.word.length // Check if it is not greater than the available letters.
-                    || allowedSpecialKeys.indexOf(keyCode) !== -1 // Special characters are always allowed.
+                    || isSpecialKey // Special characters are always allowed.
                 ) {
-                    if(allowedSpecialKeys.indexOf(keyCode) === -1) {
+                    
+                    // If it's a letter, remove from chosen array.
+                    if(!isSpecialKey) {
                         self.chosen.splice(keyIndex, 1);
                     } else {
-                        
+                        // If backspace or delete, re-add letters
+                        if(isDeleteKey) {
+                            // Look at array diff, find the difference and add back to self.chosen
+                            if(self.latestInput) {
+                                self.chosen.push(arr_diff($textInput, self.latestInput));
+                            }
+                            
+                        }
+
                     }
                     
 
@@ -81,6 +98,52 @@ $(function () {
                     console.log(keyCode, 'prevented');
                 }
             });
+
+            $('#user-input').on('keyup', function() {
+                self.latestInput = $(this).val();
+            });
+        };
+
+        Rangama.prototype.addScore = function(score) {
+            if(!isNaN(score)) {
+                this.score += score;
+            } 
+        };
+
+        Rangama.prototype.submitScore = function(playerName, totalScore) {
+            $.ajax({
+                type: "POST",
+                url: 'rangama/score/submit',
+                data: {
+                        name: playerName,
+                        score: totalScore
+                        },
+                dataType: 'json',
+            });
+        };
+
+        // Helper functions
+        function arr_diff (a1, a2) {
+
+            var a = [], diff = [];
+
+            for (var i = 0; i < a1.length; i++) {
+                a[a1[i]] = true;
+            }
+
+            for (var i = 0; i < a2.length; i++) {
+                if (a[a2[i]]) {
+                    delete a[a2[i]];
+                } else {
+                    a[a2[i]] = true;
+                }
+            }
+
+            for (var k in a) {
+                diff.push(k);
+            }
+
+            return diff;
         };
 
         // Handlebar Helpers
